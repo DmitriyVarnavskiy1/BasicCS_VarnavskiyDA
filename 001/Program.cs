b4d35929694511fda945d4e683dbd11c024b4c8c
Dmitriy <DmitriyVarnavskiy1@yandex.ru>
2022-07-09 19:46:08 +0300

    Первый семинар по C#

---
+++ 001/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+double a=3.14;
+double b;
+string? s=Console.ReadLine();//Ввод данных
+a=Convert.ToDouble(s);
+
+b=a*a;//обработка данных
+
+//Вывод результата
+System.Console.WriteLine("{0}^2={1}",a,b); //строка форматирования
+System.Console.WriteLine($"{a}^2={b}"); //строка интерполяции
+
+
+/*
+String? s;
+s=Console.ReadLine();
+Console.WhriteLine(s);
+*/
